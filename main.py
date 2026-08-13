cf37ab9357322afd5dd50c1626418bab314739c5
HamzaButt22 <hb979067@gmail.com>
2026-08-13 16:23:11 +0500

    Connected resume analysis endpoint to SQLite database via dependency injection

--- main.py
+++ main.py
@@ -5,18 +5,27 @@ from resume_analyzer import analyze_resume
 from image_analyzer import analyze_image
 import models
 from database import engine
-
+from database import SessionLocal
+from sqlalchemy.orm import Session
+from fastapi import Depends
 
 
 app = FastAPI()
 models.Base.metadata.create_all(bind=engine)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 @app.get("/")
 async def root():
     return {"message": "Hello World", "status": "FastAPI Server Running Securely"}
 
 @app.post("/analyze-resume")
-async def web_analyze_resume(file: UploadFile = File(...)):
+async def web_analyze_resume(file: UploadFile = File(...), db: Session = Depends(get_db)):
     if not file.filename.lower().endswith('.pdf'):
         raise HTTPException(status_code=400, detail="Invalid file type. Only PDF documents are permitted.")
 
@@ -27,6 +36,20 @@ async def web_analyze_resume(file: UploadFile = File(...)):
 
     try:
         category, skills = analyze_resume(temp_path)
+
+        skills_string = ", ".join(skills)
+
+        db_transaction = models.FileTransaction(
+            filename=file.filename,
+            file_type="resume",
+            status="Success",
+            result_summary=f"Category: {category} | Skills: {skills_string}"
+        )
+
+        db.add(db_transaction)
+        db.commit()
+        db.refresh(db_transaction)
+
 
         return {
             "filename": file.filename,
@@ -40,7 +63,7 @@ async def web_analyze_resume(file: UploadFile = File(...)):
             os.remove(temp_path)
 
 @app.post("/analyze-image")
-async def web_analyze_image(file: UploadFile = File(...)):
+async def web_analyze_image(file: UploadFile = File(...), db: Session = Depends(get_db)):
     allowed_extensions = ('.png', '.jpg', '.jpeg')
     if not file.filename.lower().endswith(allowed_extensions):
         raise HTTPException(status_code=400, detail="Invalid image type. Only PNG, JPG, or JPEG formats are permitted.")
